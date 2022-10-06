95f9c7e352fc5a9bf03bce984a6824d6dcf30534
Your Name <you@example.com>
2022-10-05 22:59:25 -0400

    Finished part 3

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,6 +1,10 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -15,6 +19,15 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
     @RequestMapping("")
     public String index(Model model) {
 
@@ -27,6 +40,7 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        employerRepository.findAll();
         return "add";
     }
 
@@ -38,7 +52,9 @@ public class HomeController {
             model.addAttribute("title", "Add Job");
             return "add";
         }
-
+        employerRepository.findById(employerId);
+        skillRepository.findAllById(skills);
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
